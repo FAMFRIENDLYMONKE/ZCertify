3ba0ad95276b2d28fbbeb5b9d155786c038636cb
FFM-in <178662316+FFM-in@users.noreply.github.com>
2025-07-17 01:49:43 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Badge from "./components/Badge";
+import "./App.css";
+
+function App() {
+  return <Badge />;
+}
+
+export default App;
---
+++ src/components/Badge.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Badge = () => {
+  return <div>Hello Wordl</div>;
+};
+
+export default Badge;
